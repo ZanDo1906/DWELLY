0357bc2d54f8f9657195801baf93421d1b3f8682
ZanDo1906 <94846421+ZanDo1906@users.noreply.github.com>
2026-01-30 00:26:53 +0700

    Add <done quên mật khẩu> - <van>

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -2,11 +2,11 @@ import { Routes } from '@angular/router';
 import { Test } from './test/test';
 import { PaymentMember } from './pages/payment-member/payment-member';
 import { PaymentNonMember } from './pages/payment-non-member/payment-non-member';
-// import { QRPayment } from './pages/qr-payment/qr-payment';
+import { ForgotPassword } from './pages/forgot-password/forgot-password';
 
 export const routes: Routes = [
     { path: 'test', component: Test },
     { path: 'payment-member', component: PaymentMember },
     { path: 'payment-non-member', component: PaymentNonMember },
-    // { path: 'qr-payment', component: QRPayment }
+    { path: 'forgot-password', component: ForgotPassword }
 ];
