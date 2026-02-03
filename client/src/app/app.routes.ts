8c46f94488cf72e8473b69053dff901b4f2fef1c
nhungoc-2907 <ngocltn23411@st.uel.edu.vn>
2026-02-03 22:38:41 +0700

    thêm route

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -4,11 +4,16 @@ import { PaymentMember } from './pages/payment-member/payment-member';
 import { PaymentNonMember } from './pages/payment-non-member/payment-non-member';
 import { ForgotPassword } from './pages/forgot-password/forgot-password';
 import { HomePage } from './pages/home-page/home-page';
+import { Orders } from './pages/orders/orders';
+import { OrderDetail } from './pages/order-detail/order-detail';
 
 export const routes: Routes = [
     { path: 'test', component: Test },
+    { path: '', component: HomePage }, 
     { path: 'payment-member', component: PaymentMember },
     { path: 'payment-non-member', component: PaymentNonMember },
-    { path: 'forgot-password', component: ForgotPassword }
-    { path: '', component: HomePage }, 
+    { path: 'forgot-password', component: ForgotPassword },
+    { path: 'orders', component: Orders },
+    { path: 'order-detail', component: OrderDetail },
+
 ];
