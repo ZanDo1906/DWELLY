f66bde7cb2e25dad8df49742dd59e01dc35797e5
thanhthao2512 <thaoltt23411@st.uel.edu.vn>
2026-02-24 13:56:33 +0700

    Update policy terms

--- client/src/app/pages/policy/terms/terms.ts
+++ client/src/app/pages/policy/terms/terms.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-terms',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './terms.html',
   styleUrl: './terms.css',
 })
 export class Terms {
-
+  // Component logic can be added here if needed
 }
